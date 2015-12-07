61194d62e832eb27800e4e7f43258874e0da2379
Jimmy Chen <chen.s.jimmy@gmail.com>
2015-12-07 00:37:01 -0600

    adding unit test.

--- src/test/java/org/jenkinsci/plugins/logparser/LogParserWorkflowTest.java
+++ src/test/java/org/jenkinsci/plugins/logparser/LogParserWorkflowTest.java
@@ -49,6 +49,7 @@ public class LogParserWorkflowTest {
         assertEquals(0, result.getResult().getTotalErrors());
         assertEquals(2, result.getResult().getTotalWarnings());
         assertEquals(0, result.getResult().getTotalInfos());
+        assertEquals(0, result.getResult().getBasicInfos());
     }
 
 }
