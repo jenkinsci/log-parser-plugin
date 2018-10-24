2e26583435e483c3be49fbde6dee5e52f77cdc55
Jon Brohauge <jon@brohauge.dk>
2018-10-24 21:01:11 +0200

    Actually add the test

--- src/main/java/hudson/plugins/logparser/LogParserPublisher.java
+++ src/main/java/hudson/plugins/logparser/LogParserPublisher.java
@@ -25,11 +25,14 @@ import java.util.logging.Logger;
 import jenkins.tasks.SimpleBuildStep;
 import net.sf.json.JSONObject;
 
+import org.jenkinsci.Symbol;
+
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 import org.kohsuke.stapler.StaplerRequest;
 
 public class LogParserPublisher extends Recorder implements SimpleBuildStep, Serializable {
+
     private static final long serialVersionUID = 1L;
     public boolean unstableOnWarning;
     public boolean failBuildOnError;
@@ -40,19 +43,14 @@ public class LogParserPublisher extends Recorder implements SimpleBuildStep, Ser
 
     /**
      * Create new LogParserPublisher.
-     * 
-     * @param unstableOnWarning
-     *            mark build unstable if warnings found.
-     * @param failBuildOnError
-     *            mark build failed if errors found.
-     * @param showGraphs
-     *            show graphs on job page.
-     * @param parsingRulesPath
-     *            path to the global parsing rules.
-     * @param useProjectRule
-     *            true if we use a project specific rule.
-     * @param projectRulePath
-     *            path to project specific rules relative to workspace root.
+     *
+     * @param unstableOnWarning mark build unstable if warnings found.
+     * @param failBuildOnError mark build failed if errors found.
+     * @param showGraphs show graphs on job page.
+     * @param parsingRulesPath path to the global parsing rules.
+     * @param useProjectRule true if we use a project specific rule.
+     * @param projectRulePath path to project specific rules relative to
+     * workspace root.
      */
     @Deprecated
     private LogParserPublisher(final boolean unstableOnWarning,
@@ -164,8 +162,10 @@ public class LogParserPublisher extends Recorder implements SimpleBuildStep, Ser
         return DescriptorImpl.DESCRIPTOR;
     }
 
+    @Symbol("log-parser")
     public static final class DescriptorImpl extends
             BuildStepDescriptor<Publisher> {
+
         public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
         private volatile ParserRuleFile[] parsingRulesGlobal = new ParserRuleFile[0];
         private boolean useLegacyFormatting = false;
@@ -195,17 +195,25 @@ public class LogParserPublisher extends Recorder implements SimpleBuildStep, Ser
             return parsingRulesGlobal;
         }
 
+        @DataBoundSetter
+        public void setParsingRulesGlobal(ParserRuleFile[] parsingRulesGlobal) {
+            this.parsingRulesGlobal = parsingRulesGlobal;
+        }
+
         public boolean getLegacyFormatting() {
             return useLegacyFormatting;
+        }
+
+        @DataBoundSetter
+        public void setLegacyFormatting(boolean useLegacyFormatting) {
+            this.useLegacyFormatting = useLegacyFormatting;
         }
 
         @Override
         public boolean configure(final StaplerRequest req, final JSONObject json)
                 throws FormException {
-            parsingRulesGlobal = req.bindParametersToList(ParserRuleFile.class,
-                    "log-parser.").toArray(new ParserRuleFile[0]);
-            useLegacyFormatting = json.getJSONObject("log-parser").getBoolean(
-                    "useLegacyFormatting");
+            this.useLegacyFormatting = false;
+            req.bindJSON(this, json);
             save();
             return true;
         }
@@ -229,10 +237,11 @@ public class LogParserPublisher extends Recorder implements SimpleBuildStep, Ser
     @Override
     public Action getProjectAction(AbstractProject<?, ?> project) {
 
-        if (showGraphs)
+        if (showGraphs) {
             return new LogParserProjectAction(project);
-        else
+        } else {
             return null;
+        }
     }
 
 }
--- src/test/java/org/jenkinsci/plugins/logparser/ConfigurationAsCodeTest.java
+++ src/test/java/org/jenkinsci/plugins/logparser/ConfigurationAsCodeTest.java
@@ -1,22 +1,29 @@
 package org.jenkinsci.plugins.logparser;
 
+import jenkins.model.Jenkins;
+import hudson.plugins.logparser.LogParserPublisher;
 import org.junit.Rule;
-import org.junit.rules.RuleChain;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 import io.jenkins.plugins.casc.ConfigurationAsCode;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import org.junit.Ignore;
 
 public class ConfigurationAsCodeTest {
 
     @Rule public JenkinsRule r = new JenkinsRule();
 
     @Test public void should_support_configuration_as_code() throws Exception {
+        final Jenkins jenkins = Jenkins.getInstance();
+        final LogParserPublisher.DescriptorImpl descriptor = (LogParserPublisher.DescriptorImpl) jenkins.getDescriptor(LogParserPublisher.class);
+
         ConfigurationAsCode.get().configure(ConfigurationAsCodeTest.class.getResource("configuration-as-code.yaml").toString());
         
-        assertTrue(true);
+        assertEquals(true, descriptor.getLegacyFormatting());
     }
     
+    @Ignore
     @Test public void export_configuration() throws Exception {
       ConfigurationAsCode.get().configure(ConfigurationAsCodeTest.class.getResource("configuration-as-code.yaml").toString());
       ConfigurationAsCode.get().export(System.out);
