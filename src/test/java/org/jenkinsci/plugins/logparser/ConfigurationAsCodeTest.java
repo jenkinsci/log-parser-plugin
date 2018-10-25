172c5551acd657006b0165a7e7ef82538df668e6
Jon Brohauge <jnz@topdanmark.dk>
2018-10-25 20:14:34 +0200

    Formatting?

--- src/test/java/org/jenkinsci/plugins/logparser/ConfigurationAsCodeTest.java
+++ src/test/java/org/jenkinsci/plugins/logparser/ConfigurationAsCodeTest.java
@@ -15,7 +15,8 @@ public class ConfigurationAsCodeTest {
 
     @Rule public JenkinsRule r = new JenkinsRule();
 
-    @Test public void should_support_configuration_as_code_legacy_formatter() throws Exception {
+    @Test
+    public void should_support_configuration_as_code_legacy_formatter() throws Exception {
         final Jenkins jenkins = Jenkins.getInstance();
         final LogParserPublisher.DescriptorImpl descriptor = (LogParserPublisher.DescriptorImpl) jenkins.getDescriptor(LogParserPublisher.class);
         ConfigurationAsCode.get().configure(ConfigurationAsCodeTest.class.getResource("configuration-as-code-legacy-formatting.yaml").toString());
@@ -23,20 +24,21 @@ public class ConfigurationAsCodeTest {
     }
 
     @Ignore("WIP")
-    @Test public void should_support_configuration_as_code_parsing_rules() throws Exception {
+    @Test
+    public void should_support_configuration_as_code_parsing_rules() throws Exception {
         final Jenkins jenkins = Jenkins.getInstance();
         final LogParserPublisher.DescriptorImpl descriptor = (LogParserPublisher.DescriptorImpl) jenkins.getDescriptor(LogParserPublisher.class);
 
         ConfigurationAsCode.get().configure(ConfigurationAsCodeTest.class.getResource("configuration-as-code-parsing-rules.yaml").toString());
-        System.out.println(descriptor.getParsingRulesGlobal());
         //ParserRuleFile[] parseRuleFile = descriptor.getParsingRulesGlobal();
 
         //assertEquals("Test Global Rules", parseRuleFile[0]);
     }
 
     @Ignore("Not finished")
-    @Test public void export_configuration() throws Exception {
-      ConfigurationAsCode.get().configure(ConfigurationAsCodeTest.class.getResource("configuration-as-code.yaml").toString());
-      ConfigurationAsCode.get().export(System.out);
+    @Test
+    public void export_configuration() throws Exception {
+        ConfigurationAsCode.get().configure(ConfigurationAsCodeTest.class.getResource("configuration-as-code.yaml").toString());
+        ConfigurationAsCode.get().export(System.out);
     }
 }
